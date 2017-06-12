02e265a3f7ed9e68c64a59208c1f450b1b35c7e3
Jared Dantis <jareddantis@gmail.com>
2017-06-12 22:16:00 +0800

    netlink: CVE-2016-9806

--- net/netlink/af_netlink.c
+++ net/netlink/af_netlink.c
@@ -1662,6 +1662,7 @@ static int netlink_dump(struct sock *sk)
 	struct netlink_callback *cb;
 	struct sk_buff *skb = NULL;
 	struct nlmsghdr *nlh;
+	struct module *module;
 	int len, err = -ENOBUFS;
 	int alloc_size;
 
@@ -1711,8 +1712,11 @@ static int netlink_dump(struct sock *sk)
 	if (cb->done)
 		cb->done(cb);
 	nlk->cb = NULL;
+	module = cb->module;
+	sk = cb->skb;
 	mutex_unlock(nlk->cb_mutex);
-
+    module_put(module);
+    consume_skb(skb);
 	netlink_destroy_callback(cb);
 	return 0;
 
