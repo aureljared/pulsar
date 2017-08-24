31425a8c793a0966550d1325cb32b46084ec7d08
Jared Dantis <jareddantis@gmail.com>
2017-08-25 00:16:03 +0800

    Revert "xfrm_user: validate XFRM_MSG_NEWAE incoming ESN size harder"
    This reverts commit 3814201abaf9fa4a5dcd972ade2cde09990156d9.

--- net/xfrm/xfrm_user.c
+++ net/xfrm/xfrm_user.c
@@ -375,13 +375,9 @@ static inline int xfrm_replay_verify_len(struct xfrm_replay_state_esn *replay_es
 		return 0;
 
 	up = nla_data(rp);
-	ulen = xfrm_replay_state_esn_len(up);
 
-	/* Check the overall length and the internal bitmap length to avoid
-	 * potential overflow. */
-	if (nla_len(rp) < ulen ||
-	    xfrm_replay_state_esn_len(replay_esn) != ulen ||
-	    replay_esn->bmp_len != up->bmp_len)
+	if (xfrm_replay_state_esn_len(replay_esn) !=
+			xfrm_replay_state_esn_len(up))
 		return -EINVAL;
 
 	if (up->replay_window > up->bmp_len * sizeof(__u32) * 8)
